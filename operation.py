0245d25ef46bb201471e23d9bae8afcd3d9d43aa
minercode625 <seowangduk@gmail.com>
2024-09-19 03:22:30 +0900

    Revise operation.py

--- operation.py
+++ operation.py
@@ -1,14 +1,14 @@
 def add(num1, num2):
-    return num1 + num2
+    return 0
 
 
 def subtract(num1, num2):
-    return num1 - num2
+    return 0
 
 
 def multiply(num1, num2):
-    return num1 * num2
+    return 0
 
 
 def divide(num1, num2):
-    return num1 / num2
+    return 0
